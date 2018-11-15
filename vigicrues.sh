a83451cf94d6ff65b8836bd62a1bd0f058f77e4f
Christophe Nouguier <christophe.nouguier@free.fr>
2018-11-15 12:39:29 +0100

    Fixed CMD command

--- vigicrues.sh
+++ vigicrues.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 krawler --run --port 3030 --cron "0 0 0 * * *" jobfile-stations.js &
 krawler --port 3031 --cron "0 0 * * * *" jobfile-sections.js &
-krawler --port 3032 --cron "0 0 * * * *" jobfile-stations-data.js &
+krawler --port 3032 --cron "0 0 * * * *" jobfile-stations-data.js
 
 
