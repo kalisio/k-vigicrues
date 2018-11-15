42a3c55029f306b124a88057eb1b1ce1052a632c
Christophe Nouguier <christophe.nouguier@free.fr>
2018-11-15 11:16:02 +0100

    Updated crons

--- vigicrues.sh
+++ vigicrues.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-krawler --run --port 3030 --cron "0 0 0 */1 * *" jobfile-stations.js
-krawler --port 3031 --cron "0 0 */1 * * *" jobfile-sections.js
-krawler --port 3032 --cron "0 0 */1 * * *" jobfile-stations-data.js
+krawler --run --port 3030 --cron "0 0 0 * * *" jobfile-stations.js
+krawler --port 3031 --cron "0 0 * * * *" jobfile-sections.js
+krawler --port 3032 --cron "0 0 * * * *" jobfile-stations-data.js
 
