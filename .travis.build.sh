d1ff697a9413479a152172fd1bbd94ffe6dd9f03
Christophe Nouguier <christophe.nouguier@free.fr>
2018-11-19 21:19:11 +0100

    Updates CI scripts

--- .travis.build.sh
+++ .travis.build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 source .travis.env.sh
 
+echo Building k-vigicrues $VERSION with Krawler $KRAWLER_BRANCH
+
 # Build Stations image
-docker build -f dockerfile.stations -t kalisio/k-vigicrues-stations .
+docker build --build-arg KRAWLER_BRANCH=$KRAWLER_BRANCH -f dockerfile.stations -t kalisio/k-vigicrues-stations .
 docker tag kalisio/k-vigicrues-stations kalisio/k-vigicrues:stations-$VERSION
 # Build Sections image
-docker build -f dockerfile.sections -t kalisio/k-vigicrues-sections .
+docker build --build-arg KRAWLER_BRANCH=$KRAWLER_BRANCH -f dockerfile.sections -t kalisio/k-vigicrues-sections .
 docker tag kalisio/k-vigicrues-sections kalisio/k-vigicrues:sections-$VERSION
 # Build Observations image
-docker build -f dockerfile.observations -t kalisio/k-vigicrues-observations .
+docker build --build-arg KRAWLER_BRANCH=$KRAWLER_BRANCH -f dockerfile.observations -t kalisio/k-vigicrues-observations .
 docker tag kalisio/k-vigicrues-observations kalisio/k-vigicrues:observations-$VERSION
 
 # Push the built images to Docker hub
