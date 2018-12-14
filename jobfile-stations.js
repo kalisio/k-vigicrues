413f7e4f083154e026321a4ccec3f47c380aa76c
Luc Claustres <luc.claustres@orange.fr>
2018-12-14 10:31:41 +0100

    Improved stations job to rely on more stable SANDRE WFS

--- jobfile-stations.js
+++ jobfile-stations.js
@@ -2,26 +2,34 @@ const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/vigicrues'
 
 module.exports = {
   id: 'vigicrues-stations',
-  store: 'memory',
+  store: 'fs',
   options: {
     workersLimit: 1,
     faultTolerant: false
   },
   tasks: [{
     id: 'vigicrues/stations',
-    type: 'http',
+    type: 'wfs',
     options: {
-      // https://www.data.gouv.fr/fr/datasets/stations-hydrometriques-metropole/
-      url: 'https://www.data.gouv.fr/fr/datasets/r/843df751-15eb-4871-abb1-f2e51659a697'
+      url: 'http://services.sandre.eaufrance.fr/geo/hyd',
+      version: '1.1.0',
+      typename: 'StationHydro_FXX',
+      outputFormat: 'geojson'
     }
   }],
   hooks: {
     tasks: {
       after: {
-        readJson: {},
+        unzipFromStore: {
+          input: { key: '<%= id %>', store: 'fs' },
+          output: { path: 'vigicrues', store: 'fs' }
+        },
+        readJson: {
+          key: 'vigicrues/StationHydro_FXX-geojson.dat'
+        },
         transformJson: {
           transformPath: 'features',
-          filter: { 'properties.LbAffiStaH': { $regex: '^Vigicrues' } }
+          filter: { 'properties.LbAffichageStationHydro': { $regex: '^Vigicrues' } }
         },
         writeJsonMemory: {
           hook: 'writeJson',
@@ -31,6 +39,9 @@ module.exports = {
         gzipToStore: {
           input: { key: '<%= id %>', store: 'memory' },
           output: { key: '<%= id %>', store: 'fs' }
+        },
+        deleteMongoCollection: {
+          collection: 'vigicrues-stations'
         },
         writeMongoCollection: {
           collection: 'vigicrues-stations'
@@ -53,10 +64,6 @@ module.exports = {
           // Required so that client is forwarded from job to tasks
           clientPath: 'taskTemplate.client'
         },
-        dropMongoCollection: {
-          clientPath: 'taskTemplate.client',
-          collection: 'vigicrues-stations'
-        },
         createMongoCollection: {
           clientPath: 'taskTemplate.client',
           collection: 'vigicrues-stations',
@@ -67,6 +74,12 @@ module.exports = {
         }
       },
       after: {
+        disconnectMongo: {
+          clientPath: 'taskTemplate.client'
+        },
+        removeStores: ['memory', 'fs']
+      },
+      error: {
         disconnectMongo: {
           clientPath: 'taskTemplate.client'
         },
