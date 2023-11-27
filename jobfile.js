c4cc52fc99b76f58d27ab4a484b815fa3dc0a2bb
cnouguier <ch.nouguier@gmail.com>
2023-11-27 23:19:39 +0100

    wip: Some reported values ​​are equal to 0 #34

--- jobfile.js
+++ jobfile.js
@@ -8,6 +8,61 @@ const debug = makeDebug('k-vigicrues')
 const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/vigicrues'
 const ttl = +process.env.TTL || (7 * 24 * 60 * 60)  // duration in seconds
 
+function validateFeature (feature) {
+  // Check required id/gid properties
+  if (!_.has(feature, 'properties.id') && !_.has(feature, 'properties.gid')) {
+    console.error('Invalid feature: missing id or gid property')
+    return false
+  }
+  _.set(feature, 'properties.gid', _.toNumber(feature.properties.id || feature.properties.gid))
+  _.unset(feature, 'id') // Remove unused ID if any
+  // Check required LbEntCru property
+  if (!_.has(feature, 'properties.LbEntCru')) {
+    console.warn('Invalid feature: missing \'LbEntCru\' property')
+    return false
+  }
+  _.set(feature, 'properties.name', feature.properties.LbEntCru) // needed for timeseries
+  // Check required NivInfViCr property
+  if (!_.has(feature, 'properties.NivInfViCr' )) {
+    console.warn('Invalid feature: missing \'NivInfViCr\' property')
+    return false
+  }
+  // Ensure the value is clamped to [1-4]
+  // see https://github.com/kalisio/k-vigicrues/issues/34
+  if (feature.properties.NivInfViCr < 1) {
+    console.warn(`Value for ${feature.properties.name} is invalid: ${nivSituVigiCruEnt}. Setting the value to '1'`)
+    _.set(feature, 'properties.NivInfViCr', 1)
+  }
+  if (feature.properties.NivInfViCr > 4) {
+    console.warn(`Value for ${feature.properties.name} is invalid: ${nivSituVigiCruEnt}. Setting the value to '3'`)
+    _.set(feature, 'properties.NivInfViCr', 4)
+  }
+  // Ensure the geometry is clean as some line strings have degenerated lines
+  if (getType(feature) === 'MultiLineString') {
+    let validLines = []
+    let nbInvalidGeometries = 0
+    featureEach(flatten(feature), line => {
+      try {
+        cleanCoords(line, { mutate: true })
+        validLines.push(getCoords(line))
+      } catch (error) {
+        nbInvalidGeometries++
+      }
+    })
+    if (nbInvalidGeometries > 0) debug(`Filtering ${nbInvalidGeometries} invalid line(s) for ${feature.properties.LbEntCru}`)
+    // Rebuild geometry from the clean line
+    feature.geometry = multiLineString(validLines).geometry
+  } else if (getType(feature) === 'LineString')  {
+    try {
+      cleanCoords(feature, { mutate: true })
+    } catch (error) {
+      console.warn(`Invalid geometry for ${feature.properties.LbEntCru}`)
+      return false
+    }
+  }
+  return true
+}
+
 export default {
   id: 'vigicrues',
   store: 'memory',
@@ -25,45 +80,16 @@ export default {
   hooks: {
     tasks: {
       after: {
-        readJson: {
-        },
+        readJson: {},
         generateStations: {
           hook: 'apply',
           function: (item) => {
             const features = _.get(item, 'data.features', [])
             let validFeatures = []
             _.forEach(features, feature => {
-              // Ensure clean geometry as some line strings have degenerated lines
-              if (getType(feature) === 'MultiLineString') {
-                let validLines = []
-                let nbInvalidGeometries = 0
-                featureEach(flatten(feature), line => {
-                  try {
-                    cleanCoords(line, { mutate: true })
-                    validLines.push(getCoords(line))
-                  } catch (error) {
-                    nbInvalidGeometries++
-                  }
-                })
-                if (nbInvalidGeometries > 0) debug(`Filtering ${nbInvalidGeometries} invalid line(s) for ${feature.properties.LbEntCru}`)
-                // Rebuild geometry from the clean line
-                feature.geometry = multiLineString(validLines).geometry
-                validFeatures.push(feature)
-              } else if (getType(feature) === 'LineString')  {
-                try {
-                  cleanCoords(feature, { mutate: true })
-                  validFeatures.push(feature)
-                } catch (error) {
-                  debug(`Filtering invalid line for ${feature.properties.LbEntCru}`)
-                }
-              }
-              // Convert ID to numeric value. 
-              // Note: Keep gid for backward compatibility (https://github.com/kalisio/k-vigicrues/issues/33)
-              _.set(feature, 'properties.gid', _.toNumber(feature.properties.id || feature.properties.gid))
-              // Remove unused ID if any
-              _.unset(feature, 'id')
-              _.set(feature, 'properties.name', feature.properties.LbEntCru) // needed for timeseries
+              if (validateFeature(feature)) validFeatures.push(feature)
             })
+            console.log(`Found ${validFeatures.length} features`)
             _.set(item, 'data.features', validFeatures)
           }
         },
@@ -79,7 +105,7 @@ export default {
               'properties.gid',
               'properties.name',
               'properties.CdEntCru',
-              'properties.CdTCC'
+              'properties.CdTCC',
             ],
             inPlace: false
           },
@@ -95,7 +121,8 @@ export default {
               _.set(forecastFeature, 'time', moment.utc().toDate())
               _.set(forecastFeature, 'properties.gid', feature.properties.gid) // needed for timeseries
               _.set(forecastFeature, 'properties.name', feature.properties.LbEntCru) // needed for timeseries
-              _.set(forecastFeature, 'properties.NivSituVigiCruEnt', feature.properties.NivInfViCr) // needed for timeseries
+              _.set(forecastFeature, 'properties.NivSituVigiCruEnt', feature.properties.NivInfViCr) // backward compatibilty
+              _.set(forecastFeature, 'properties.risk', feature.properties.NivInfViCr)
               forecastFeatures.push(forecastFeature)
             })
             item.data = forecastFeatures
